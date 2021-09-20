fde358ef3aba11978eee44dc1675cc46adbf6b16
SgtFloW <florian.georg.weismann@gmail.com>
2021-09-20 14:51:34 +0200

    Updated project & added eslint

--- index.js
+++ index.js
@@ -1,24 +1,36 @@
-const chalk = require("chalk");
-const colorList = ["#ff0000", "#666666", "#999999", "#efefef", "#b7b7b7", "#444444"];
-const col = 31;
-const row = 12;
+const chalk = require('chalk');
+const colorList = [
+  '#ff0000',
+  '#666666',
+  '#999999',
+  '#efefef',
+  '#b7b7b7',
+  '#444444',
+];
 
-if (process.argv.length > 2) {
-    let input = process.argv[2];
-    let str = "";
-    for (let i = 0; i < col; i++) {
-        str = str + "#";
-    }
-    for (let i = 0; i < row; i++) {
-        console.log(chalk.hex(input)(str));
-    }
+if (process.argv.length === 3) {
+  console.log(
+    chalk.keyword(process.argv[2])(`    ###############################
+    ###############################
+    ###############################
+    ###############################
+    ###          ${process.argv[2]}         ###
+    ###############################
+    ###############################
+    ###############################p
+    ###############################`),
+  );
 } else {
-    let nbr = colorList[Math.floor(Math.random() * colorList.length)];
-    let str = "";
-    for (let i = 0; i < col; i++) {
-        str = str + "#";
-    }
-    for (let i = 0; i < row; i++) {
-        console.log(chalk.hex(nbr)(str));
-    }
+  const nbr = colorList[Math.floor(Math.random() * colorList.length)];
+  console.log(
+    chalk.hex(nbr)(`    ###############################
+    ###############################
+    ###############################
+    ###############################
+    ###          ${nbr}        ###
+    ###############################
+    ###############################
+    ###############################
+    ###############################`),
+  );
 }
